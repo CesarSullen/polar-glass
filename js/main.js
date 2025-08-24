ea0c172392abd71f147c1436edf7a23365f8b562
Cesar <cesarsuarez2303@gmail.com>
2025-08-24 16:32:21 +0200

    animations and UI changes

--- js/main.js
+++ js/main.js
@@ -29,3 +29,22 @@ links.forEach((link) =>
 		toggleBtn.classList.toggle("toggled");
 	})
 );
+
+// Interception Observer
+const animatedElements = document.querySelectorAll(
+	".show-up, .show-down, .show-left, .show-right, .bounce-in, .rotate-left, .rotate-right"
+);
+
+const observer = new IntersectionObserver(
+	(entries, observer) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add("animated");
+				observer.unobserve(entry.target);
+			}
+		});
+	},
+	{ root: null, rootMargin: "0px", threshold: 0.2 }
+);
+
+animatedElements.forEach((el) => observer.observe(el));
